2df946b2d5bd8fba54602cd22685f35f788f8710
Johee Chung <johee@candescenthealth.com>
2017-10-22 00:02:33 -0500

    Set state with JSON Why: * Using React, this will assist in iterating through the JSON object This change addresses the need by: * Adding stage-2 to babel inorder to use the spread operator * Setting the state with JSON

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
 //Requires a proxy otherwise response is missing Access-Control-Allow-Origin header.
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const url = 'https://d10xkldqejj5hr.cloudfront.net/dev/dashboard.json';
 
-fetch(proxyUrl + url)
-  .then(response => {
-  });
+const root = document.createElement('div');
+document.body.append(root);
 
+class Videos extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+    }
+  }
+
+  render() {
+    return (
+      <div>Hi</div>
+    );
+  }
+
+  componentDidMount() {
+    fetch(proxyUrl + url)
+      .then(response => {
+        response.json()
+          .then(content => {
+            this.setState({...content});
+            console.log(this.state);
+            return this.state['loading'];
+          })
+          .then(loadingStatus => {
+            if (loadingStatus) {
+              this.setState({loading: false});
+            }
+          })
+          .catch(() => {
+            console.log('Unable to set state with JSON');
+          });
+      })
+      .catch(() => {
+        console.log('Error in fetching JSON');
+      });
+  }
+}
+
+ReactDom.render(<Videos />, root);
